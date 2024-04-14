e17b2908b7825a73677ca58e703af512eceaa681
Tamás Molnár <tamas.molnar@4host.hu>
2024-04-14 22:51:45 +0200

    update

--- task01/deploy_vm.ps1
+++ task01/deploy_vm.ps1
@@ -2,7 +2,7 @@
 az group create --name "ChemaxonTest" --location swedencentral
 
 #Deploy VM
-az vm create --resource-group "ChemaxonTest" --name "AzureVM1" --image Win2019Datacenter --assign-identity --admin-username "azureuser" --admin-password "GNN5=#-@-5qt3gan"
+az vm create --resource-group "ChemaxonTest" --name "AzureVM1" --image Win2019Datacenter --assign-identity --admin-username "azureuser" --admin-password "uHVRmpJR9ocJkbAy"
 
 #Install AzureAD extension to the VM
 az vm extension set --publisher Microsoft.Azure.ActiveDirectory --name AADLoginForWindows --resource-group "ChemaxonTest" --vm-name "AzureVM1"
